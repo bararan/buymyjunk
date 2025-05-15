bd9ee45721aa181c8d45fb40e98f5c73c4233f93
baran <bararan@gmail.com>
2025-05-15 16:48:38 -0400

    Removed formsets.

--- messaging/views.py
+++ messaging/views.py
@@ -5,7 +5,6 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView
 from .models import Message, Thread
 from .forms import MessageForm
-from django.forms import inlineformset_factory
 
 # Create your views here.
 
@@ -13,18 +12,22 @@ from django.forms import inlineformset_factory
 def messagebox_view(req):
     user = req.user
     inbox = Thread.objects.filter(users__in=[user]).all()
-    ReplyFormSet = inlineformset_factory(Thread, Message, form=MessageForm, extra=len(inbox))
-    threads_w_forms = []
-    for thread in inbox:
-        threads_w_forms.append([thread, ReplyFormSet(req.POST,instance=thread)])
+    # ReplyFormSet = inlineformset_factory(Thread, Message, form=MessageForm, extra=len(inbox))
+    # threads_w_forms = []
+    # for thread in inbox:
+    #     threads_w_forms.append([thread, ReplyFormSet(req.POST,instance=thread)])
 # TODO: The validation below is not correct. Think how to do it properly.
+    msg_form = MessageForm(data=req.POST or None)
+    active_thread = inbox[0]
     if req.method == 'POST':
-        if reply_form.is_valid():
-            reply.thread = thread
+        if msg_form.is_valid():
+            message = message_form.save(commit=False)
+            message.thread = thread
             reply = reply_form.save()
     context = {
         'username': user.username,
-        'inbox': threads_w_forms,
+        'inbox': inbox,
+        'active_thread': inbox[0] or None
         # 'reply_forms': reply_forms,
     }
     return render(req, 'messaging/messagebox.html', context)
